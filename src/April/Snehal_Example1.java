5482f5d55e7d70d834ff7ae94956edce49fccc94
Admin <Admin@DESKTOP-FQA9A7D>
2021-08-02 22:54:39 +0530

    Snehal_Example1 created

---
+++ src/April/Snehal_Example1.java
@@ -0,0 +1,16 @@
+package April;
+
+public class Snehal_Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		{
+			System.out.println("welcome to github.");
+		}
+
+
+
+	}
+
+}
